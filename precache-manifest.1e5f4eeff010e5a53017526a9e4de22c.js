420f2ce4ae78f594d38bec846e12121bb5d06fb2
vik123 <vikrawat11@gmail.com>
2019-09-21 16:20:30 +0530

    Updates

---
+++ precache-manifest.1e5f4eeff010e5a53017526a9e4de22c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a789a41fad57c785ac53cba2590f7270",
+    "url": "/monster-rolodex/index.html"
+  },
+  {
+    "revision": "1cb4fd02fdbec9215863",
+    "url": "/monster-rolodex/static/css/main.f2207f8b.chunk.css"
+  },
+  {
+    "revision": "f7abcd3b9d99ae6d26ef",
+    "url": "/monster-rolodex/static/js/2.bffc00c9.chunk.js"
+  },
+  {
+    "revision": "1cb4fd02fdbec9215863",
+    "url": "/monster-rolodex/static/js/main.4d0c0f15.chunk.js"
+  },
+  {
+    "revision": "a786790ecc9dd156c966",
+    "url": "/monster-rolodex/static/js/runtime~main.c1466fcc.js"
+  }
+]);
